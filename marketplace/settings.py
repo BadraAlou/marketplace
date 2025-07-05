0b43d3aa4aac351ef1dadd6371a10ea7694cd376
Badra <badra10alou10@gmail.com>
2025-07-05 23:12:10 +0000

    static fix

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -101,7 +101,7 @@ STATIC_URL = 'static/'
 #STATICFILES_DIRS = [BASE_DIR / 'static']
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
